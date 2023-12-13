d87be0658471e9ccb16d95b4d0760ffb2c369b82
Paulo Cataneo <69061663+paulocataneo@users.noreply.github.com>
2023-12-13 01:22:22 +0000

    edição do arquivo index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 function Home() {
-    return <h1>Teste</h1>
-
+    return (
+        <div style={{ textAlign: 'center', margin: '20px', fontFamily: 'Arial, sans-serif' }}>
+            <h1 style={{ marginBottom: '10px', fontSize: '24px', fontWeight: 'bold', color: '#333' }}>
+                Se você está visualizando esta página, significa que alguém que te ama muito está no caminho certo para se tornar um 💻 DEV 💻 de respeito e que muito em breve será perdoado por todas aquelas noites nas quais você perdeu o sono 💤 escutando os 🔊 "tic tac" do meu teclado.
+            </h1>
+            <p style={{ fontSize: '18px', color: '#555' }}>
+                Te amo, neguinha❤️, obrigado por me ajudar a se tornar a minha melhor versão!
+            </p>
+        </div>
+    );
 }
 
 export default Home;
